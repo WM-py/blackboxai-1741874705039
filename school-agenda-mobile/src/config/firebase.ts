c5b47bdd6426538384a8cd4262434e7de68c697e
Watylla Macedo <83824090+WM-py@users.noreply.github.com>
2025-03-13 11:05:12 -0300

    Add school-agenda-mobile/src/config/firebase.ts 1741874711535

---
+++ school-agenda-mobile/src/config/firebase.ts
@@ -0,0 +1,9 @@
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import messaging from '@react-native-firebase/messaging';
+
+// Initialize Firebase services
+export { auth, firestore, messaging };
+
+// Export default auth for convenience
+export default auth;
