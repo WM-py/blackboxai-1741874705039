1a37fdab06f1724546a14ad33b93d74bfcb2d697
Watylla Macedo <83824090+WM-py@users.noreply.github.com>
2025-03-13 11:05:43 -0300

    Add school-agenda-mobile/constants/Colors.ts 1741874743454

---
+++ school-agenda-mobile/constants/Colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = '#007AFF';
+const tintColorDark = '#0A84FF';
+
+export default {
+  light: {
+    text: '#000000',
+    background: '#FFFFFF',
+    tint: tintColorLight,
+    tabIconDefault: '#666666',
+    tabIconSelected: tintColorLight,
+    border: '#E0E0E0',
+  },
+  dark: {
+    text: '#FFFFFF',
+    background: '#000000',
+    tint: tintColorDark,
+    tabIconDefault: '#666666',
+    tabIconSelected: tintColorDark,
+    border: '#333333',
+  },
+};
